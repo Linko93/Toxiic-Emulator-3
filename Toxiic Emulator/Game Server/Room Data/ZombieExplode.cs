b68364b9484fe09f78e4a48c1f851f6ba5ab8128
Linko93 <135510884+Linko93@users.noreply.github.com>
2023-06-04 12:29:09 +0200

    Emulator

---
+++ Toxiic Emulator/Game Server/Room Data/ZombieExplode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game_Server.Room_Data
+{
+    class RoomHandler_ZombieExplode : RoomDataHandler
+    {
+        public override void Handle(User usr, Room room)
+        {
+
+        }
+    }
+}
